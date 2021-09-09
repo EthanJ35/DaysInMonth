8369b955061a5ca7dd7bc0ad79d498a95c8b4767
EthanJ35 <85588248+EthanJ35@users.noreply.github.com>
2021-09-09 04:00:41 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -26,8 +26,6 @@ public class Main {
 
     static void daysInMonth(int month, int year)
     {
-        int[] intDaysInMonth = new int[] {28, 29, 30, 31};
-
         if(month<1 || month>12 || year<1 || year>9999)
         {
             System.out.println(-1);
@@ -35,18 +33,18 @@ public class Main {
         switch(month)
         {
         case 1: case 3: case 5: case 7: case 8: case 10: case 12:
-            System.out.println(intDaysInMonth[3]);
+            System.out.println(31);
             break;
         case 2:
             if(isLeapYear(year))
             {
-                System.out.println(intDaysInMonth[1]);
+                System.out.println(29);
                 break;
             }
-            System.out.println(intDaysInMonth[0]);
+            System.out.println(28);
             break;
         case 4: case 6: case 9: case 11:
-            System.out.println(intDaysInMonth[2]);
+            System.out.println(30);
             break;
         }
     }
